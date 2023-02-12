a8a1510dd2647ca9d197ee71cee07d9b46613c9d
bikuu <bikuu@github.com>
2023-02-12 19:02:00 +0545

    Finish building stop watch

--- script.js
+++ script.js
@@ -43,3 +43,64 @@ hours.innerHTML = h;
 minutes.innerHTML = m;
 seconds.innerHTML = s;
 ampm.innerHTML = am;
+
+// Stop Watch
+
+let stopWatchsec = 00;
+let tens = 00;
+let stopWatchmin = 00;
+let stopWatchhr = 00;
+
+let getTens = document.querySelector(".stop-watch-wrapper .time .tens");
+let getSeconds = document.querySelector(".stop-watch-wrapper .time .seconds");
+let getMinute = document.querySelector(".stop-watch-wrapper .time .minute");
+let getHour = document.querySelector(".stop-watch-wrapper .time .hour");
+
+let btnStart = document.querySelector(".btn-start");
+let btnStop = document.querySelector(".btn-stop");
+let btnReset = document.querySelector(".btn-reset");
+let interval;
+function stopWatch(btn) {
+  if (btn === "btn-start") {
+    startTimer();
+  } else if (btn === "btn-stop") {
+    stopTimer();
+  } else if (btn === "btn-reset") {
+    resetTimer();
+  }
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    tens++;
+    tens = tens < 10 ? "0" + tens : tens;
+    getTens.innerHTML = tens;
+    if (tens > 99) {
+      stopWatchsec++;
+      stopWatchsec = stopWatchsec < 10 ? "0" + stopWatchsec : stopWatchsec;
+      tens = "00";
+      getTens.innerHTML = tens;
+      getSeconds.innerHTML = stopWatchsec;
+    }
+    if (stopWatchsec > 59) {
+      stopWatchsec = "00";
+      getMinute.innerHTML = stopWatchsec;
+      stopWatchmin++;
+      stopWatchmin = stopWatchmin < 10 ? "0" + stopWatchmin : stopWatchmin;
+      getMinute.innerHTML = stopWatchmin;
+    }
+  }, 10);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+function resetTimer() {
+  clearInterval(interval);
+  tens = "00";
+  getTens.innerHTML = tens;
+  stopWatchsec = "00";
+  getSeconds.innerHTML = stopWatchsec;
+  stopWatchmin = "00";
+  getMinute.innerHTML = stopWatchmin;
+}

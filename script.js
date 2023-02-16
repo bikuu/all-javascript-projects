5f9183b93f16a42c7e413d6a73dd978f67cd81cf
bikuu <bikuu@github.com>
2023-02-16 17:39:00 +0545

    Finish project Temperature Calculator

--- script.js
+++ script.js
@@ -193,3 +193,32 @@ function calculateBMI() {
   }
 }
 btn.addEventListener("click", calculateBMI);
+
+// Tempature Convertor
+
+const celsius = document.querySelector("#celsius");
+const fahrenheit = document.querySelector("#fahrenheit");
+const kelvin = document.querySelector("#kelvin");
+
+function convertTemp(event) {
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celsius":
+      fahrenheit.value = (currentValue * 1.8 + 32).toFixed(2);
+      kelvin.value = (currentValue + 273.15).toFixed(2);
+      break;
+
+    case "fahrenheit":
+      celsius.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvin.value = ((currentValue - 32) / 1.8 + 273.15).toFixed(2);
+      break;
+    case "kelvin":
+      celsius.value = (currentValue - 273.15).toFixed(2);
+      fahrenheit.value = ((currentValue - 273.15) * 1.8 + 32).toFixed(2);
+      break;
+
+    default:
+      break;
+  }
+}
